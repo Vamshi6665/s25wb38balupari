047fcda31b76aacbbf0525353f58b58ca5b3587e
Vamshi Krishna B <vamshi6665@gmail.com>
2025-04-01 15:32:38 -0500

    changes

--- routes/grid.js
+++ routes/grid.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET grid page. */
-router.get('/grid', function(req, res, next) {
-  // Get query parameters from the URL
-  const { rows, cols } = req.query;
-
-  // Pass the query parameters to the template
+// Route to handle the grid page
+router.get('/', function(req, res, next) {
+  const { rows, cols } = req.query;  // Get query parameters
   res.render('grid', { title: 'Grid Display', query: { rows, cols } });
 });
 
