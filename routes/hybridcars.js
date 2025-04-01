dcd12a6aada2ff1a315422080818f17bb632a2cc
Vamshi Krishna B <vamshi6665@gmail.com>
2025-04-01 14:53:36 -0500

    new changes

--- routes/hybridcars.js
+++ routes/hybridcars.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET hybrid cars page. */
 router.get('/', function(req, res, next) {
-  res.render('hybridcars', { title: 'Search Results hybridcars' });
+  res.render('hybridcars', { title: 'Search Results - Hybrid Cars' });
 });
 
 module.exports = router;
