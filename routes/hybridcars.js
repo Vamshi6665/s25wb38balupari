6604c618efc30ab7caf9b41df7a99388b919d9c4
Vamshi Krishna B <vamshi6665@gmail.com>
2025-04-01 15:02:04 -0500

    new changes

--- routes/hybridcars.js
+++ routes/hybridcars.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET hybrid cars page. */
 router.get('/', function(req, res, next) {
-  res.render('hybridcars', { title: 'Search Results - Hybrid Cars' });
+  // Define the results array in the backend
+  var results = [
+    { name: "Toyota Prius", mileage_mpg: 56, type: "Hybrid" },
+    { name: "Honda Insight", mileage_mpg: 55, type: "Hybrid" },
+    { name: "Ford Fusion Hybrid", mileage_mpg: 42, type: "Hybrid" }
+  ];
+  
+  // Pass the results array to the view
+  res.render('hybridcars', { title: 'Search Results - Hybrid Cars', results: results });
 });
 
 module.exports = router;
