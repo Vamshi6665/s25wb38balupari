8bcf9c2d4f790c72d0da6702d3f96a8af2b54938
Vamshi Krishna B <vamshi6665@gmail.com>
2025-04-01 16:23:10 -0500

    changes to the files

--- routes/pick.js
+++ routes/pick.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pick', { title: 'A random item' });
+  res.render('randomitem', { title: 'A random item' });
 });
 
 module.exports = router;
