92bcaf8869f0151eaf8259631eedaf73a5152653
Vamshi Krishna B <vamshi6665@gmail.com>
2025-04-01 16:04:02 -0500

    changes to the files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hybridCarsRouter = require('./routes/hybridcars');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hybridcars', hybridCarsRouter);
 app.use('/grid', gridRouter);
+app.use('/pick', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
