35c0db04d508c664c3afbff16055584e80a3e72e
Vamshi Krishna B <vamshi6665@gmail.com>
2025-04-01 15:21:48 -0500

    new changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hybridCarsRouter = require('./routes/hybridcars');
+var gridRouter = require('./routes/grid');
 
 
 
@@ -22,9 +23,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/hybridcars', hybridCarsRouter);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hybridcars', hybridCarsRouter);
+app.use('/grid', gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
